4d3951c1a3ffa85d5c000e2b9f724aafff0623fb
Sebastian Neuser <haggl@sineband.de>
2017-08-15 02:16:38 +0200

    Add Telegram support

--- hasibot.py
+++ hasibot.py
@@ -10,22 +10,32 @@ from textwrap import dedent
 
 from yaml import safe_load
 from sleekxmpp import ClientXMPP
+from telegram.ext import Filters, MessageHandler, Updater
 
 class HaSiBot(ClientXMPP):
     """A fairly simple bot that relays messages from HaSi's IRC channel (mapped
     to a special MUC on the XMPP server) to the HaSi MUC and vice versa.
     """
 
-    def __init__(self, jid, password, irc_room, xmpp_room):
+    def __init__(self, config):
         """Initializes the bot.
+
+        Args:
+            config      the YAML configuration
         """
-        ClientXMPP.__init__(self, jid, password)
+        ClientXMPP.__init__(self, config['jid'], config['pw'])
         self.nick = 'hasibot'
-        self.irc_room = irc_room
-        self.xmpp_room = xmpp_room
+        self.irc_room = config['irc']
+        self.xmpp_room = config['xmpp']
+        self.tg_chat = config['tg_chat']
 
         self.add_event_handler("session_start", self.sign_in)
         self.add_event_handler("groupchat_message", self.forward_message)
+
+        self._tg_updater = Updater(config['tg_token'])
+        self._tg_dispatcher = self._tg_updater.dispatcher
+        self._tg_dispatcher.add_handler(MessageHandler(Filters.text, self.handle_telegram_message))
+        self._tg_updater.start_polling()
 
         # Connect to the XMPP server and start processing XMPP stanzas.
         if self.connect():
@@ -33,6 +43,16 @@ class HaSiBot(ClientXMPP):
             print("Done")
         else:
             print("Unable to connect.")
+
+    def handle_telegram_message(self, bot, update):
+        """Dispatcher handler that forwards messages from the Telegram group chat to IRC and XMPP.
+        """
+        msg = update.message
+        # Ignore messages from other chats than the configured one
+        if msg.chat.id != self.tg_chat:
+            return
+        self._send_xmpp_message(self.irc_room, msg.from_user.name, msg.text)
+        self._send_xmpp_message(self.xmpp_room, msg.from_user.name, msg.text)
 
     def sign_in(self, event):
         """
@@ -61,28 +81,37 @@ class HaSiBot(ClientXMPP):
                    for stanza objects and the Message stanza to see
                    how it may be used.
         """
+        author = msg['mucnick']
+        text = msg['body']
+
         # Do not bounce forwarded messages back and forth
-        if msg['mucnick'] == self.nick:
+        if author == self.nick:
             debug('Ignoring message from %s', self.nick)
             return
 
         room = msg['from'].jid
         room = room[:room.index('/')]
-        body = '<{}> {}'.format(msg['mucnick'], msg['body'])
         if room == self.irc_room:
             info('Relaying message from %s', self.irc_room)
-            # Remove tag from telegram bot
-            if match(r'<\W*shurely\d*>', body):
-                body = body[body.index('>')+2:]
-            self.send_message(mto=self.xmpp_room, mbody=body, mtype='groupchat')
+            self._send_xmpp_message(self.xmpp_room, author, text)
         elif room == self.xmpp_room:
             info('Relaying message from %s', self.xmpp_room)
-            self.send_message(mto=self.irc_room, mbody=body, mtype='groupchat')
+            self._send_xmpp_message(self.irc_room, author, text)
         else:
             warning('Ignoring message from unkown source: %s', room)
             return
 
-def prepare_argument_parser():
+        self._tg_updater.bot.send_message(chat_id=self.tg_chat, text=_format_message(author, text))
+
+    def _send_xmpp_message(self, room, author, text):
+        body = '<{}> {}'.format(author, text)
+        self.send_message(mto=room, mbody=body, mtype='groupchat')
+
+
+def _format_message(author, text):
+    return '<{}> {}'.format(author, text)
+
+def _prepare_argument_parser():
     """Creates and configures the argument parser for the CLI.
     """
     parser = ArgumentParser(description='HaSi XMPP-to-IRC bridge bot.',
@@ -104,7 +133,7 @@ def prepare_argument_parser():
 def main():
     """Well... The main() method. Nuff said.
     """
-    args = prepare_argument_parser().parse_args()
+    args = _prepare_argument_parser().parse_args()
     if args.log:
         logging.basicConfig(filename=args.log, format='%(levelname)-8s %(message)s',
                             level=logging.INFO)
@@ -116,7 +145,7 @@ def main():
         data = conf_file.read()
         config = safe_load(data)
 
-    HaSiBot(config['jid'], config['pw'], config['irc'], config['xmpp'])
+    HaSiBot(config)
 
 if __name__ == '__main__':
     main()
